5a8cad4ecbc948244187f95c95642fb71406fb01
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-08-09 18:44:06 +0300

    Discpline is the key

---
+++ web-dev/AngelaYu-complete-web-development-bootcamp/1-javascript/play-ground/script.js
@@ -0,0 +1,5 @@
+$("h1").addClass("margin-r");
+console.log($("h1").hasClass("margin-r"));
+
+$("h1").removeClass("margin-r");
+console.log($("h1").hasClass("margin-r"));
---
+++ web-dev/frontend-engineer/02-JavaScriptRefresher-usingTS/objects.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env ts-node
+
+/* class Person {
+  constructor(public name: string, public age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const p = new Person("Bob", 20);
+console.log(p); */

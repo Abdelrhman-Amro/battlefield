a8eb3c74eb0b4154026963ab52b43d9e35778e94
“Abdelrhman-Amro” <abdamro288dev@gmail.com>
2025-01-09 11:12:39 +0200

    Master Django #🚀

---
+++ python/Django/0x02-library_management_system/lms/lms_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, "pages/index.html")
+
+
+def books(request):
+    return render(request, "pages/books.html")
